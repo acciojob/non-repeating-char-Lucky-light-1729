233412d9ba618902ffce0b3ec8177f939e0c1e44
Lucky-light-1729 <agrawalanay20@gmail.com>
2025-06-04 13:29:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	const freqMap = {};
+	for (let char of str) 
+	{
+		freqMap[char] = (freqMap[char] || 0) + 1;
+	}
+	for (let char of str) 
+	{
+		if (freqMap[char] === 1) 
+		{
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
